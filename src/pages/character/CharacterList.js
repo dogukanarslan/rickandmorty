5db182a0eba7a62647c2b465c845508a1b6f319d
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-05-25 21:46:23 +0300

    Add dark theme to characters pagination buttons

--- src/pages/character/CharacterList.js
+++ src/pages/character/CharacterList.js
@@ -1,7 +1,7 @@
 import {useContext} from 'react';
 import {ThemeContext} from '../../contexts/ThemeContext';
 
-const CharacterList = ({ characters, setCharacters }) => {
+const CharacterList = ({characters, setCharacters}) => {
   const handleClick = (type) => {
     fetch(characters.info[type])
       .then((response) => response.json())
@@ -14,39 +14,39 @@ const CharacterList = ({ characters, setCharacters }) => {
 
   return (
     <div>
-      <table className={`table table-hover ${isLightTheme ? 'text-dark' : 'text-light' }`}>
+      <table className={`table table-hover ${isLightTheme ? 'text-dark' : 'text-light'}`}>
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>Status</th>
-            <th>Species</th>
-            <th>Type</th>
-            <th>Gender</th>
-          </tr>
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Species</th>
+          <th>Type</th>
+          <th>Gender</th>
+        </tr>
         </thead>
         <tbody>
-          {characters.results &&
-            characters.results.map((character) => {
-              return (
-                <tr key={character.id}>
-                  <td>{character.name}</td>
-                  <td>{character.status}</td>
-                  <td>{character.species}</td>
-                  <td>{character.type}</td>
-                  <td>{character.gender}</td>
-                </tr>
-              );
-            })}
+        {characters.results &&
+        characters.results.map((character) => {
+          return (
+            <tr key={character.id}>
+              <td>{character.name}</td>
+              <td>{character.status}</td>
+              <td>{character.species}</td>
+              <td>{character.type}</td>
+              <td>{character.gender}</td>
+            </tr>
+          );
+        })}
         </tbody>
       </table>
       <nav>
         <ul className="pagination d-flex justify-content-between">
           <li className="page-item" onClick={() => handleClick('prev')}>
-            <button className="page-link">Previous</button>
+            <button className={`btn ${isLightTheme ? 'btn-dark' : 'btn-light'}`}>Previous</button>
           </li>
           <span>{characters.info && characters.info.page}</span>
           <li className="page-item" onClick={() => handleClick('next')}>
-            <button className="page-link">Next</button>
+            <button className={`btn ${isLightTheme ? 'btn-dark' : 'btn-light'}`}>Next</button>
           </li>
         </ul>
       </nav>
