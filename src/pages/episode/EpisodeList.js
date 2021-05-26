e6d078f846835d2a4ee749c75e26e71a5b73bd68
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-05-26 21:54:06 +0300

    Add dark theme to episodes pagination buttons

--- src/pages/episode/EpisodeList.js
+++ src/pages/episode/EpisodeList.js
@@ -38,11 +38,11 @@ const EpisodeList = ({ episodes, setEpisodes }) => {
       <nav>
         <ul className="pagination d-flex justify-content-between">
           <li className="page-item" onClick={() => handleClick('prev')}>
-            <button className="page-link">Previous</button>
+            <button className={`btn ${isLightTheme ? 'btn-dark' : 'btn-light'}`}>Previous</button>
           </li>
           <span>{episodes.info && episodes.info.page}</span>
           <li className="page-item" onClick={() => handleClick('next')}>
-            <button className="page-link">Next</button>
+            <button className={`btn ${isLightTheme ? 'btn-dark' : 'btn-light'}`}>Next</button>
           </li>
         </ul>
       </nav>
