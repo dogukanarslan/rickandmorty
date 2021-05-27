c4c256b8d96a7de7b465d738ba37d246a9f8b0f9
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-05-27 23:32:17 +0300

    Highlight active list item

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,25 @@
-import {Link} from "react-router-dom";
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Sidebar = () => {
+  const [selectedGroup, setSelectedGroup] = useState(0);
+
+  const isSelected = (index) => {
+    return selectedGroup === index;
+  };
+
   return (
     <div className="list-group">
-      <Link to="/characters" className="list-group-item list-group-item-action">
+      <Link onClick={() => setSelectedGroup(0)} to="/characters"
+            className={`list-group-item list-group-item-action ${isSelected(0) ? 'active' : ''}`}>
         Characters
       </Link>
-      <Link to="/locations" className="list-group-item list-group-item-action">
+      <Link onClick={() => setSelectedGroup(1)} to="/locations"
+            className={`list-group-item list-group-item-action ${isSelected(1) ? 'active' : ''}`}>
         Locations
       </Link>
-      <Link to="/episodes" className="list-group-item list-group-item-action">
+      <Link onClick={() => setSelectedGroup(2)} to="/episodes"
+            className={`list-group-item list-group-item-action ${isSelected(2) ? 'active' : ''}`}>
         Episodes
       </Link>
     </div>
