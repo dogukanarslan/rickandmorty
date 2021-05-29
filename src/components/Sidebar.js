0133959124f39a5062608bff189723ff0401ce6c
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-05-29 14:42:36 +0300

    Customize sidebar styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,32 @@
-import {useState} from 'react';
+import {useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {ThemeContext} from '../contexts/ThemeContext';
 
 const Sidebar = () => {
   const [selectedGroup, setSelectedGroup] = useState(0);
+
+  const {isLightTheme} = useContext(ThemeContext)
 
   const isSelected = (index) => {
     return selectedGroup === index;
   };
 
   return (
-    <div className="list-group">
-      <Link onClick={() => setSelectedGroup(0)} to="/characters"
-            className={`list-group-item list-group-item-action ${isSelected(0) ? 'active' : ''}`}>
-        Characters
-      </Link>
-      <Link onClick={() => setSelectedGroup(1)} to="/locations"
-            className={`list-group-item list-group-item-action ${isSelected(1) ? 'active' : ''}`}>
-        Locations
-      </Link>
-      <Link onClick={() => setSelectedGroup(2)} to="/episodes"
-            className={`list-group-item list-group-item-action ${isSelected(2) ? 'active' : ''}`}>
-        Episodes
-      </Link>
+    <div className={`${isLightTheme ? 'sidebar-light' : 'sidebar-dark'}`}>
+      <div className="sidebar">
+        <Link onClick={() => setSelectedGroup(0)} to="/characters"
+              className={`sidebar-item  ${isSelected(0) ? 'active' : ''}`}>
+          Characters
+        </Link>
+        <Link onClick={() => setSelectedGroup(1)} to="/locations"
+              className={`sidebar-item  ${isSelected(1) ? 'active' : ''}`}>
+          Locations
+        </Link>
+        <Link onClick={() => setSelectedGroup(2)} to="/episodes"
+              className={`sidebar-item  ${isSelected(2) ? 'active' : ''}`}>
+          Episodes
+        </Link>
+      </div>
     </div>
   );
 };
