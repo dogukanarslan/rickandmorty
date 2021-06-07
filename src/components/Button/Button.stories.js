0486fe3df6b4e1440491f6cebec4c2d9442b9790
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-06-07 21:52:10 +0300

    Add arguments to button stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,8 +1,16 @@
 import { StyledButton } from './Button.styles';
 
-export const LightButton = () => <StyledButton label="Button" />;
+export const DarkButton = (args) => <StyledButton {...args} />;
+DarkButton.args = {
+  label: 'Dark Button',
+  dark: true,
+};
 
-export const DarkButton = () => <StyledButton dark label="Button" />;
+export const LightButton = (args) => <StyledButton {...args} />;
+LightButton.args = {
+  label: 'Light Button',
+  dark: false,
+};
 
 export default {
   title: 'components/Button',
