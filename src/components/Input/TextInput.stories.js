8e23e4ac55fbfc16f4d893f10eb6de9d1265f61b
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-06-09 21:49:11 +0300

    Add text input story

---
+++ src/components/Input/TextInput.stories.js
@@ -0,0 +1,12 @@
+import { StyledTextInput } from './Input.styles';
+
+export const TextInput = (args) => <StyledTextInput {...args} />;
+TextInput.args = {
+  label: 'Text Input',
+  placeholder: 'Input',
+};
+
+export default {
+  title: 'components/TextInput',
+  component: StyledTextInput,
+};
