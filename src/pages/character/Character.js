1cdee2a5dced271bd57176fc6b4f7a4f0bb73c9f
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-06-14 20:53:21 +0300

    Send request on initial character loading

--- src/pages/character/Character.js
+++ src/pages/character/Character.js
@@ -1,25 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { api } from '../../constants';
 import CharacterList from './CharacterList';
 import Filter from './CharacterFilter';
 import { CharacterContext } from '../../contexts/CharacterContext';
+import { Loader } from '../../GlobalStyles';
 
 const Character = () => {
   const { characters, setCharacters } = useContext(CharacterContext);
+  const [isLoading, setIsLoading] = useState(true);
+
   const getCharacters = (filter = '') => {
     fetch(`${api}/character/?${filter}`)
       .then((res) => res.json())
       .then((data) => {
         setCharacters(data);
+        setIsLoading(false);
       });
   };
 
-  return (
-    <div>
-      <Filter getCharacters={getCharacters} />
-      <CharacterList characters={characters} setCharacters={setCharacters} />
-    </div>
-  );
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  } else {
+    return (
+      <div>
+        <Filter getCharacters={getCharacters} />
+        <CharacterList characters={characters} setCharacters={setCharacters} />
+      </div>
+    );
+  }
 };
 
 export default Character;
