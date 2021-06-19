8c191c29e37891defc6781322d104eac24fa0fd1
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-06-19 23:05:59 +0300

    Add custom scrollbar css

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -25,6 +25,24 @@ export const GlobalStyle = createGlobalStyle`
       text-decoration: underline;
     }
   }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${orange};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #b30000;
+  }
 `;
 
 export const Container = styled.div`
